5ac64ca450a1321d785f850fab3b507577c2e8e4
Tokichi <gladyrev_a@mail.ru>
2017-09-30 17:21:47 +0400

    HW3

--- HomeWork/HomeWork03/Task1.java
+++ HomeWork/HomeWork03/Task1.java
@@ -1,18 +1,26 @@
 package ru.itpark;
 
-public class Main {
+public class gg {
     public static int parse(char digits[]) {
         int itog = 0;
         for ( int i = 0; i < digits.length; i++){
-            int a = digits.length - 1 - i;
-            int b = (int)Math.pow(10, a);
+            int x = digits.length - 1 - i;
+            int b = stepen(x);
             itog = itog + (digits[i] - 48) * b;
         }
         return itog;
     }
 
+    public static int stepen(int x) {
+        int a = 1;
+        for (int i = 0; i < x; i++) {
+            a = a * 10;
+        }
+        return a;
+    }
+
     public static void main(String[] args) {
-        char number[] = {'1', '2', '6', '7'};
+        char number[] = {'1', '2', '6', '7',};
         int numberAsInt = parse(number);
         System.out.println(numberAsInt);
 
